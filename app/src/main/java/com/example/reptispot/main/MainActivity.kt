12e73523129620cedc65466f04599782aada8a15
morozovvd <v.d.morozov@yandex.ru>
2019-03-31 13:32:41 +0300

    camera usage

--- app/src/main/java/com/example/reptispot/main/MainActivity.kt
+++ app/src/main/java/com/example/reptispot/main/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.reptispot.main
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.reptispot.R
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     // Flag to indicate which task is to be performed.
     private val REQUEST_SELECT_IMAGE = 0
+    private val REQUEST_TAKE_PHOTO = 1
 
     private lateinit var viewModel: MainViewModel
+    private var currentPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +32,16 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProviders.of(this)
             .get(MainViewModel::class.java)
 
-        //get photo from album
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "image/*"
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivityForResult(intent, REQUEST_SELECT_IMAGE)
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            dispatchTakePictureIntent()
+        } else {
+            //todo: wrap into dispatch method
+            //get photo from album
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, REQUEST_SELECT_IMAGE)
+            }
         }
     }
 
@@ -35,8 +51,60 @@ class MainActivity : AppCompatActivity() {
                 // If image is selected successfully, set the image URI and bitmap.
                 data?.data?.let { viewModel.onImageSelected(it) }
             }
+            REQUEST_TAKE_PHOTO -> if (resultCode == Activity.RESULT_OK) {
+                galleryAddPic()
+                viewModel.onImageSelected(Uri.fromFile(File(currentPhotoPath)))
+            }
             else -> {
             }
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.reptispot.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
         }
     }
 }
